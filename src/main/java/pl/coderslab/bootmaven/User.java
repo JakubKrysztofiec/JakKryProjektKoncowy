028822517c6bcefd7a8b22f0580698ab52131045
jakub <jakub.krysztofiec@wp.pl>
2023-06-23 22:56:13 +0200

    1

---
+++ src/main/java/pl/coderslab/bootmaven/User.java
@@ -0,0 +1,31 @@
+package pl.coderslab.bootmaven;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+}
---
+++ src/test/java/pl/coderslab/bootmaven/UserRepositoryTests.java
@@ -0,0 +1,36 @@
+package pl.coderslab.bootmaven;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setEmail("jakub.krysztofiec@wp.pl");
+        user.setPassword("12345");
+        user.setName("Jakub");
+
+        User savedUser = userRepository.save(user);
+
+        User existUser = entityManager.find(User.class, savedUser.getId());
+
+        assertThat(user.getEmail()).isEqualTo(existUser.getEmail());
+    }
+}
