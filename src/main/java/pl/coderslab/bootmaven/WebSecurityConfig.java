472b193892c10611b89bf4d7328ca0ff03bc3f84
jakub <jakub.krysztofiec@wp.pl>
2023-06-27 13:51:33 +0200

    Commit 2

--- src/main/java/pl/coderslab/bootmaven/WebSecurityConfig.java
+++ src/main/java/pl/coderslab/bootmaven/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package pl.coderslab.bootmaven;
 
+import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,14 +16,19 @@ public class WebSecurityConfig{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/","/register").permitAll()
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests((authorize) -> authorize
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
                 )
                 .formLogin((form) -> form
                         .loginPage("/login")
                         .permitAll()
+                        .defaultSuccessUrl("https://www.google.com", true)
                 )
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/register", "/process_register", "/webjars/bootstrap/css/bootstrap.min.css", "/webjars/jquery/jquery.min.js", "/webjars/bootstrap/js/bootstrap.min.js").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
                 .logout((logout) -> logout.logoutUrl("/logout").permitAll());
 
         return http.build();
